863bc26e9780e949c022502c69dc5ab08d38ec17
DONGHEE KIM <donghee653@gmail.com>
2023-05-31 22:28:28 +0900

    Initial commit

---
+++ src/main/java/com/join/member/controller/MemberController.java
@@ -0,0 +1,65 @@
+package com.join.member.controller;
+
+import com.join.member.dto.MemberDTO;
+import com.join.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Member;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    // 생성자 주입
+    private final MemberService memberService;
+
+
+    // 회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            // login 실패
+            return "login";
+        }
+    }
+
+    @GetMapping("/member/")     //회원정보 가져오기.
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 위의 model을 사용한다.
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
+    }
+
+
+}
